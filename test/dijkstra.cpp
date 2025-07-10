a8103a9ef1c7a8aa27cf592f238ab7dffcdd62ce
1nchy <wangyinqi23@mails.ucas.ac.cn>
2025-07-10 15:26:41 +0800

    feat(test): test multigraph in dijkstra and floyd

--- test/dijkstra.cpp
+++ test/dijkstra.cpp
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-template <typename _Cost, typename _Vk> auto dijstra_trail(
+template <typename _Cost, typename _Vk> auto dijkstra_trail(
     const _Vk& _x, const _Vk& _y,
     const std::unordered_map<_Vk, std::pair<_Cost, _Vk>>& _dijk,
     std::function<void(const _Vk&)>&& _trailer
@@ -17,7 +17,7 @@ template <typename _Cost, typename _Vk> auto dijstra_trail(
         return;
     }
     if (!_dijk.contains(_y)) { return; }
-    dijstra_trail(_x, _dijk.at(_y).second, _dijk, std::move(_trailer));
+    dijkstra_trail(_x, _dijk.at(_y).second, _dijk, std::move(_trailer));
     _trailer(_y);
 }
 
@@ -52,7 +52,7 @@ ICY_CASE("graph") {
         });
         EXPECT_FALSE(_from_a.contains("F"));
         std::vector<std::string> _trail;
-        dijstra_trail<unsigned, std::string>("A", "B", _from_a, [&_trail](const std::string& _k) -> void {
+        dijkstra_trail<unsigned, std::string>("A", "B", _from_a, [&_trail](const std::string& _k) -> void {
             _trail.push_back(_k);
         });
         std::vector<std::string> _expected_trail = {"A", "C", "D", "B"};
@@ -64,7 +64,7 @@ ICY_CASE("graph") {
         });
         EXPECT_EQ(_from_f.size(), 5);
         std::vector<std::string> _trail;
-        dijstra_trail<unsigned, std::string>("F", "A", _from_f, [&_trail](const std::string& _k) -> void {
+        dijkstra_trail<unsigned, std::string>("F", "A", _from_f, [&_trail](const std::string& _k) -> void {
             _trail.push_back(_k);
         });
         std::vector<std::string> _expected_trail = {"F", "D", "B", "C", "A"};
@@ -72,11 +72,63 @@ ICY_CASE("graph") {
     }
 }
 ICY_CASE("multigraph") {
-    icy::multigraph<std::string, void, unsigned> _g;
+    icy::multigraph<std::string, char, unsigned> _g;
     using vertex_type = typename decltype(_g)::vertex_type;
     using edge_type = typename decltype(_g)::edge_type;
     using key_type = typename decltype(_g)::key_type;
     using cost_type = unsigned;
     using vertex_visitor = typename decltype(_g)::vertex_visitor<cost_type>;
     using edge_visitor = typename decltype(_g)::edge_visitor<cost_type>;
+    _g.insert("A", 'a');
+    _g.insert("B", 'B');
+    _g.insert("C", 'c');
+    _g.insert("D", 'd');
+    _g.insert("E", 'E');
+    _g.insert("F", 'f');
+    _g.insert("G", 'G');
+    _g.insert("H", 'h');
+    _g.connect("A", "B", 3);
+    _g.connect("B", "C", 15);
+    _g.connect("B", "E", 12);
+    _g.connect("C", "F", 18);
+    _g.connect("D", "A", 5);
+    _g.connect("D", "E", 18);
+    _g.connect("E", "D", 10);
+    _g.connect("E", "F", 25);
+    _g.connect("E", "G", 9);
+    _g.connect("E", "G", 14);
+    _g.connect("F", "F", 3);
+    _g.connect("F", "G", 8);
+    _g.connect("G", "D", 11);
+    _g.connect("G", "H", 6);
+    ICY_SUBCASE("from A") {
+        const auto _from_a = _g.dijkstra<unsigned>("A", [](const edge_type& _e) -> unsigned {
+            return _e.value();
+        });
+        EXPECT_EQ(_from_a.size(), 7);
+        std::vector<std::string> _trail;
+        dijkstra_trail<unsigned, std::string>("A", "H", _from_a, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {"A", "B", "E", "G", "H"};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+    ICY_SUBCASE("from G") {
+        const auto _from_g = _g.dijkstra<unsigned>("G", [](const edge_type& _e) -> unsigned {
+            return _e.value();
+        });
+        EXPECT_EQ(_from_g.size(), 7);
+        std::vector<std::string> _trail;
+        dijkstra_trail<unsigned, std::string>("G", "F", _from_g, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {"G", "D", "A", "B", "C", "F"};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+    ICY_SUBCASE("from H") {
+        const auto _from_h = _g.dijkstra<unsigned>("H", [](const edge_type& _e) -> unsigned {
+            return _e.value();
+        });
+        EXPECT_EQ(_from_h.size(), 0);
+    }
 }
--- test/floyd.cpp
+++ test/floyd.cpp
@@ -53,7 +53,6 @@ ICY_CASE("graph") {
         floyd_trail<unsigned, std::string>("A", "B", _floyd, [&_trail](const std::string& _k) -> void {
             _trail.push_back(_k);
         });
-        EXPECT_EQ(_trail.size(), 4);
         std::vector<std::string> _expected_trail = {"A", "C", "D", "B"};
         EXPECT_EQ(_trail, _expected_trail);
     }
@@ -62,8 +61,73 @@ ICY_CASE("graph") {
         floyd_trail<unsigned, std::string>("F", "A", _floyd, [&_trail](const std::string& _k) -> void {
             _trail.push_back(_k);
         });
-        EXPECT_EQ(_trail.size(), 5);
         std::vector<std::string> _expected_trail = {"F", "D", "B", "C", "A"};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+    ICY_SUBCASE("from B to F") {
+        std::vector<std::string> _trail;
+        floyd_trail<unsigned, std::string>("B", "F", _floyd, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+}
+ICY_CASE("multigraph") {
+    icy::multigraph<std::string, char, unsigned> _g;
+    using vertex_type = typename decltype(_g)::vertex_type;
+    using edge_type = typename decltype(_g)::edge_type;
+    using key_type = typename decltype(_g)::key_type;
+    using cost_type = unsigned;
+    using vertex_visitor = typename decltype(_g)::vertex_visitor<cost_type>;
+    using edge_visitor = typename decltype(_g)::edge_visitor<cost_type>;
+    _g.insert("A", 'a');
+    _g.insert("B", 'B');
+    _g.insert("C", 'c');
+    _g.insert("D", 'd');
+    _g.insert("E", 'E');
+    _g.insert("F", 'f');
+    _g.insert("G", 'G');
+    _g.insert("H", 'h');
+    _g.connect("A", "B", 3);
+    _g.connect("B", "C", 15);
+    _g.connect("B", "E", 12);
+    _g.connect("C", "F", 18);
+    _g.connect("D", "A", 5);
+    _g.connect("D", "E", 18);
+    _g.connect("E", "D", 10);
+    _g.connect("E", "F", 25);
+    _g.connect("E", "G", 9);
+    _g.connect("E", "G", 14);
+    _g.connect("F", "F", 3);
+    _g.connect("F", "G", 8);
+    _g.connect("G", "D", 11);
+    _g.connect("G", "H", 6);
+    const auto _floyd = _g.floyd<unsigned>([](const edge_type& _e) -> unsigned {
+        return _e.value();
+    });
+    ICY_SUBCASE("from A to H") {
+        std::vector<std::string> _trail;
+        floyd_trail<unsigned, std::string>("A", "H", _floyd, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {"A", "B", "E", "G", "H"};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+    ICY_SUBCASE("from G to F") {
+        std::vector<std::string> _trail;
+        floyd_trail<unsigned, std::string>("G", "F", _floyd, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {"G", "D", "A", "B", "C", "F"};
+        EXPECT_EQ(_trail, _expected_trail);
+    }
+    ICY_SUBCASE("from H to D") {
+        std::vector<std::string> _trail;
+        floyd_trail<unsigned, std::string>("H", "D", _floyd, [&_trail](const std::string& _k) -> void {
+            _trail.push_back(_k);
+        });
+        std::vector<std::string> _expected_trail = {};
         EXPECT_EQ(_trail, _expected_trail);
     }
 }
